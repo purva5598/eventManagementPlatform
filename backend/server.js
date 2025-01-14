4517f6ec4fd7f72e6cbd981738fe74176b3184c8
purva sharma <sharmapurva5598@gmail.com>
2025-01-14 18:05:53 +0530

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const notificationRoutes = require('./routes/notifications');
+const eventRoutes = require('./routes/eventRoutes');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
+
+app.use('/api/events', eventRoutes);
+app.use('/api/notifications', notificationRoutes);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
